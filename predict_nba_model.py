d320af891ec9d3fe205f2abac2b8e80a81e13bba
Fahad Hussain <fahad.hussain2@ontariotechu.net>
2025-02-17 17:25:17 -0500

    Implement Feature Encoders

--- predict_nba_model.py
+++ predict_nba_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 import os
 
 # Function to load and preprocess the data
@@ -30,8 +31,37 @@ def create_roster(df):
     ).to_dict()
     return roster
 
+# Encode all categorical features to numerical values 
+def encode_features(df, player_encoder, team_encoder, season_encoder):
+    df['home_team_encoded'] = team_encoder.transform(df['home_team'])
+    df['away_team_encoded'] = team_encoder.transform(df['away_team'])
+    df['season_encoded'] = season_encoder.transform(df['season'])
+    for i in range(5):
+        df[f'home_{i}_encoded'] = player_encoder.transform(df[f'home_{i}'])
+        df[f'away_{i}_encoded'] = player_encoder.transform(df[f'away_{i}'])
+    return df
+
+
 data_dir = './csv_files'
 df = load_and_preprocess_data(data_dir) # Dataframe to store data from all csv files
 
 # Create dictionary with rosters for each team
 roster_dict = create_roster(df)
+
+# Initialize encoders
+player_encoder = LabelEncoder()
+team_encoder = LabelEncoder()
+season_encoder = LabelEncoder()
+
+# Fit all the encoders
+all_players = pd.unique(df[[f'home_{i}' for i in range(5)] + [f'away_{i}' for i in range(5)]].values.ravel())
+player_encoder.fit(all_players)
+
+teams = pd.unique(pd.concat([df['home_team'], df['away_team']]))
+team_encoder.fit(teams)
+
+seasons = pd.unique(df['season'])
+season_encoder.fit(seasons)
+
+# Encode the dataframe
+df = encode_features(df, player_encoder, team_encoder, season_encoder)
